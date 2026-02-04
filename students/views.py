8277b4e18a8616901474135cf18272bc30d083bc
Theertha-Hari <theerthahari990@gmail.com>
2026-02-04 11:12:53 +0530

    Added student list page with detail and edit links

--- students/views.py
+++ students/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Student
 
 def studentlist(request):
     students = Student.objects.all()
     return render(request, 'student_list.html', {'students': students})
 
+def student_detail(request,id):
+    student=get_object_or_404(Student,id=id)
+    return render(request,'student_detail.html',{'student':student})
